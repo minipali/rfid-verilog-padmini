4e5c1102f480ceeebb201cc26f16ddcf4440d2fd
minipali <96111566+minipali@users.noreply.github.com>
2023-03-09 20:25:02 +0530

    Update crc16check.v

--- tag/crc16check.v
+++ tag/crc16check.v
@@ -34,6 +34,31 @@ always @ (posedge crcinclk or posedge reset) begin
   end // ~reset
 end // always
 
+
+always @ (negedge reset) begin
+  if (reset) begin
+    crc     <= 16'hFFFF;
+  end else begin
+    crc[0]  <= crcbitin ^ crc[15];
+    crc[1]  <= crc[0];
+    crc[2]  <= crc[1];
+    crc[3]  <= crc[2];
+    crc[4]  <= crc[3];
+    crc[5]  <= crc[4] ^ crcbitin ^ crc[15];
+    crc[6]  <= crc[5];
+    crc[7]  <= crc[6];
+    crc[8]  <= crc[7];
+    crc[9]  <= crc[8];
+    crc[10] <= crc[9];
+    crc[11] <= crc[10];
+    crc[12] <= crc[11] ^ crcbitin ^ crc[15];
+    crc[13] <= crc[12];
+    crc[14] <= crc[13];
+    crc[15] <= crc[14];
+    
+  end // ~reset
+end // always
+
 endmodule
 
 
