fc9c6959021764077ddfecad57559aacef0dc21e
minipali <96111566+minipali@users.noreply.github.com>
2023-04-01 21:00:00 +0530

    Update crc5check.v

--- tag/crc5check.v
+++ tag/crc5check.v
@@ -1,64 +1,47 @@
-//final as of 26-03-2023
-
-
+//final as of 1/4/23
 
 `timescale 1ns/1ns
-
 // CRC5
-
 // inclk, inbit generate crc
-
-
-module crc5check(masterreset, reset, crcinclk, crcbitin, crc);
-input masterreset, reset, crcinclk, crcbitin;
-output reg [4:0] crc;
-
-
+module crc5check(reset, crcinclk, crcbitin, crcout);
+input  reset, crcinclk, crcbitin;
+output wire [4:0] crcout;
+reg [4:0] crc;
 //reg [2:0] bitoutcounter; //8 bit counter
 //wire crcdone;
 //assign crcout = crc;
 //assign crcbitout = 0;
-
 //initial crc     <= 5'b01001;
 
-always @ (masterreset or reset or crcinclk) begin
-  if(masterreset || reset) begin
-    crc     <= 5'b01001;
-//~reset below because want it triggered at negedge of crc5reset, and crcinclk because only want posedge crcclk 
-  end else if(crcinclk & ~reset) begin 
-    crc[0]  <= (crcbitin ^ crc[4]);
-    crc[1]  <= crc[0];
-    crc[2]  <= crc[1];
-    crc[3]  <= crc[2] ^ crcbitin ^ crc[4];
-    crc[4]  <= crc[3];    
-  end else begin
-    //do nothing
-    //crc <= crc;
-  end// ~reset
+assign	crcout[0] = crc[0] || reset;
+assign	crcout[1] = crc[1] & ~reset;
+assign	crcout[2] = crc[2] & ~reset;
+assign	crcout[3] = crc[3] || reset;
+assign	crcout[4] = crc[4] & ~reset;
+
+always @ (posedge crcinclk) begin
+    crc[0]  <= (crcbitin ^ crcout[4]);
+    crc[1]  <= crcout[0];
+    crc[2]  <= crcout[1];
+    crc[3]  <= crcout[2] ^ crcbitin ^ crcout[4];
+    crc[4]  <= crcout[3];
+
 end // always
 
 endmodule
 
 /*
 `timescale 1ns/1ps
-
 // CRC5
-
 // inclk, inbit generate crc
-
-
 module crc5check (reset, crcinclk, crcbitin, crc);
 input reset, crcinclk, crcbitin;
 output reg [4:0] crc;
-
-
 //reg [2:0] bitoutcounter; //8 bit counter
 //wire crcdone;
 //assign crcout = crc;
 //assign crcbitout = 0;
-
 //initial crc     <= 5'b01001;
-
 always @ ( posedge crcinclk or posedge reset) begin
   if (reset) begin
     crc     <= 5'b01001;
@@ -68,10 +51,8 @@ always @ ( posedge crcinclk or posedge reset) begin
     crc[2]  <= crc[1];
     crc[3]  <= crc[2] ^ crcbitin ^ crc[4];
     crc[4]  <= crc[3];
-    
   end // ~reset
 end // always
-
 always @ (negedge reset) begin
     if(reset) begin
         crc     <= 5'b01001;
@@ -80,11 +61,8 @@ always @ (negedge reset) begin
        crc[1]  <= crc[0];
        crc[2]  <= crc[1];
        crc[3]  <= crc[2] ^ crcbitin ^ crc[4];
-       crc[4]  <= crc[3]; 
+       crc[4]  <= crc[3];
     end
 end
-
 endmodule
 */
-
-
