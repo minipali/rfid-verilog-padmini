d7780e296384733b8c142857f675865f5ec6fde7
yasaswini2401 <99539932+yasaswini2401@users.noreply.github.com>
2023-05-15 12:43:56 +0530

    Update top_MC_mem.v

--- tag/top_MC_mem.v
+++ tag/top_MC_mem.v
@@ -1,173 +1,111 @@
-//final as of 26-04-2023
-
-
-`timescale 1ns / 1ns
+`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/09/2023 05:54:31 PM
+// Design Name: 
+// Module Name: mem_always_on
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module top_MC_mem(
-            //top and memory
-           input reset, clk, 
-           
-           //directly top
-           input demodin, 
-           input use_q,
-           input comm_enable,
-           input [3:0] debug_address,
-           
-           
-           //directly to memory
-           input factory_reset,
-           input ADC_data_ready, 
-           input [7:0] ADC_data, 
-           input [15:0] mem_read_in,
-           
-           //directly from memory
-           output wire [15:0] mem_data_out, //data is given to the memory
-           output wire PC_B,WE,SE,
-           output wire [5:0] mem_address,  //this will enable WL
-           output wire [2:0] mem_sel,
-           
-           
-           //directly from top
-           output wire modout, // regular IO
-           output wire tx_enable,
-           output wire debug_out,
-           output wire osc_enable_pll,
-           output wire pll_enable,
-           output wire [3:0] freq_channel,
-           output wire rforbase,
-           output wire [2:0] senscode,
-           output wire morb_trans,
-           output wire [7:0] bf_dur,
-           output wire backscatter_const,
-           output wire bitout, calibration_control,
-           output wire packet_complete);
-         
-         
-//from top op to mem ip
-    wire [13:0] rx_cmd;
-    wire [2:0] sel_target, sel_action;
-    wire [15:0] mask;   
-    wire [1:0] readwritebank;
-    wire [7:0] readwriteptr;
-    wire [7:0] readwords;
-    wire [15:0] EPC_data_in;
-    wire EPC_data_ready;
-    wire [7:0]sensor_time_stamp;    
-    wire membitclk;
-    
-// from mem op to top ip
-    wire memdatadone; 
-    wire  membitsrc;
-    wire sl_flag;//from memory to top
-    
-    //redundant
-    wire mem_done;
-    wire inven_flag;
-    wire [1:0] session;
-    
-   
-    top topinst(reset, clk, demodin, modout, // regular IO
-           EPC_data_in, EPC_data_ready,
-           readwritebank, readwriteptr, readwords, membitclk, membitsrc, memdatadone, 
-           use_q, comm_enable, tx_enable,
-           debug_address, debug_out,
-           rx_cmd,
-           ///transmit clock
-           osc_enable_pll, pll_enable, freq_channel, rforbase,
-           ////from sample data
-           senscode,
-           /////
-           morb_trans, sensor_time_stamp,
-           bf_dur, backscatter_const,      
-           bitout,
-           calibration_control,
-           //select
-           sel_target, sel_action, mask,
-           sl_flag, packet_complete);
-//module top(reset, clk, demodin, modout, // regular IO
-//           writedataout, epc_data_ready,
-//           readwritebank, readwriteptr, readwords, membitclk, membitsrc, memdatadone, 
-//           use_q, comm_enable, tx_enable,
-//           debug_address, debug_out,
-//           rx_cmd,
-//           ///transmit clock
-//           osc_enable_pll, pll_enable, freq_channel, rforbase,
-//           ////from sample data
-//           senscode,
-//           /////
-//           morb_trans, sensor_time_stamp,
-//           bf_dur, backscatter_const,      
-//           bitout,
-//           calibration_control,
-//           //select
-//           sel_target, sel_action, sel_ptr, mask,
-//           sl_flag, packet_complete);        
-           
-     mem m1(clk,factory_reset,reset,packet_complete,
-                rx_cmd,
-                sel_target,
-                sel_action,
-                mask,  
-                readwritebank,
-                readwriteptr, 
-                readwords,
-                EPC_data_in,
-                ADC_data_ready,
-                EPC_data_ready,     
-                ADC_data,
-                senscode, // 3-bit flag to indicate the 3 sensors
-                mem_read_in,  //data from memory
-                sensor_time_stamp,   
-                membitclk,
-                tx_enable,
-            //outputs
-                mem_data_out, //data is given to the memory
-                PC_B,WE,SE,
-                mem_address,   //this will enable WL
-                mem_sel,
-                membitsrc,
-                mem_done,
-                sl_flag,
-                inven_flag,
-                session,
-                memdatadone);       
-//module mem(
-//    input wire clk,factory_reset,reset,packetcomplete,
-//    input wire [12:0] rx_cmd,
-//    input wire [2:0] sel_target,
-//    input wire [2:0] sel_action
-//    input wire [15:0] mask,  
-//    input wire [1:0] readwritebank,
-//    input wire [7:0] readwriteptr, 
-//    input wire [7:0] readwords,
-//    input wire [15:0]EPC_data_in,
-//    input wire ADC_data_ready, 
-//    input wire EPC_data_ready,    
-//    input wire [7:0] ADC_data,
-//    input wire [2:0]sensor_code, // 3-bit flag to indicate the 3 sensors
-//    input wire [15:0]mem_read_in,  //data from memory
-//    input wire [7:0]sensor_time_stamp,   
-//    input wire data_clk,
-//    input wire tx_enable,
+module mem_always_on(
+input wire [13:0]rx_cmd,
+input wire packet_complete,tx_enable,ADC_data_ready,
+input wire clk,factory_reset,current_inven_flag_in,current_sl_flag_in,
+input wire [15:0]Code1_in,
+input wire [5:0]Counter_EPC_in,Counter_s1_in,Counter_s2_in,
+output reg [5:0]Counter_EPC_out,Counter_s1_out,Counter_s2_out,
+output reg current_inven_flag_out,current_sl_flag_out,
+output reg [15:0]Code1_out
+);
+//Used flags so that the output and input is updated inn one clock cycle
+reg in_flag,out_flag;
+reg current_inven_flag,current_sl_flag;
+reg [15:0] Code1;
+reg [5:0]Counter_EPC,Counter_s1,Counter_s2;
 
-//    output reg [15:0] mem_data_out, //data is given to the memory
-//    output reg PC_B,WE,SE,
-//    output reg [5:0]mem_address,   //this will enable WL
-//    output reg [2:0]mem_sel,
-//    output reg tx_bit_src,
-//    output reg mem_done,
-//    output reg sl_flag,inven_flag,
-//    output reg [1:0]session,
-//    output reg tx_data_done
-//);
+always@(posedge clk)begin
+if(factory_reset)begin
+    Counter_EPC = 6'd0;
+    Counter_s1 = 6'd0;
+    Counter_s2 = 6'd0;
+    current_inven_flag = 1'd1;
+    current_sl_flag = 1'd1;
+    Code1 = 16'd0;
+    out_flag = 1'd0;
+    in_flag = 1'd0;
+    Counter_EPC_out = 6'd0;
+    Counter_s1_out = 6'd0;
+    Counter_s2_out = 6'd0;
+    current_inven_flag_out = 1'd1;
+    current_sl_flag_out = 1'd1;
+    Code1_out = 16'd0;
+end else begin
+//for select command - here Code1 is the word that we will match with the mask
+    if(Counter_EPC == 6'd3)begin
+        Code1 = Code1_in;
+    end
+//This is also for select command - here we store inven and sl flags 
+    if(rx_cmd[4])begin
+        if(!packet_complete)begin
+            current_sl_flag_out = current_sl_flag;
+            current_inven_flag_out = current_inven_flag;
+            Code1_out = Code1;   
+        end else begin
+            current_sl_flag = current_sl_flag_in;
+            current_inven_flag = current_inven_flag_in;
+        end
+    end   
+//This is for read command - On reading Counter_s1,Counter_s2 will be updated(decrements)
+    if(rx_cmd[11])begin
+        Counter_s1_out = Counter_s1;
+        Counter_s2_out = Counter_s2; 
+        in_flag = 1'd1;   
+    end 
+    if(!tx_enable&&(in_flag == 1'd1))begin
+        in_flag = 1'd0;
+        Counter_s1 = Counter_s1_in;
+        Counter_s2 = Counter_s2_in;
+    end     
+//EPC_read and this is for ACK command is independent of Counter_EPC.
+     if(rx_cmd[1])begin
+        Counter_EPC_out = Counter_EPC;   
+    end     
     
-    
-    
-    
-    
-    
-    
+//This is for sensor write    
+    if(rx_cmd[10] && (packet_complete))begin
+        if(!ADC_data_ready)begin
+        Counter_s1_out = Counter_s1;
+        Counter_s2_out = Counter_s2;
+        end else begin
+        Counter_s1 = Counter_s1_in;
+        Counter_s2 = Counter_s2_in;
+        end
+    end  
+//This is for EPC_write    
+    if(rx_cmd[8]&& (!packet_complete)&&(out_flag == 1'd0))begin
+        Counter_EPC_out = Counter_EPC;
+        out_flag = 1'd1;
+        in_flag = 1'd1;
+    end 
+    if(rx_cmd[8] &&(in_flag == 1'd1))begin
+        Counter_EPC = Counter_EPC_in;
+        in_flag = 1'd0;
+        out_flag = 1'd0;      
+    end
+
+end
+end
 endmodule
